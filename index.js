06b7e3bc2adf646a4ff222c191db2b3871befa48
Kurstyn Storms <kurstyn.storms@powerhrg.com>
2021-05-25 10:26:38 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const names = [ 'Lisa', 'Kaitlin', 'Jan' ]
+
+function writeCards(names, surprise) {
+    const cards = []
+    for (let i = 0; i < names.length; i++){
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`)
+    }
+    return cards
+}
+function countDown(number){
+    let integer = number 
+    while (integer >= 0) {
+    console.log(integer--)
+    }
+}   
